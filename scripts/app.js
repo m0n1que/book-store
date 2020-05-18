e08b3db62a0c8f0bf0c84acbc0aee3eec1cfd293
Monika <jovanova02monika@gmail.com>
2020-05-18 16:53:23 +0200

    initial commit
    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,44 @@
+class Author {
+    constructor(fName, lName) {
+        this.fName = fName;
+        this.lName = lName;
+    }
+}
+
+class Book extends Author{
+    title = '';
+    publishedDate = new Date(); 
+    picture = '';
+    totalPages; 
+    publisher; 
+    language; 
+    category;
+    price = 0;
+
+    constructor(fName, lName, title, publishedDate, picture, totalPages, publisher, language, category, price) {
+        super(fName, lName);
+        this.title = title;
+        this.publishedDate = new Date(); 
+        this.picture = picture;
+        this.totalPages = totalPages; 
+        this.publisher = publisher; 
+        this.language = language;
+        this.category = category;
+        this.price = price;
+    }
+}
+
+const chargin = new Book(
+    'Honore',
+    'de Balzac',
+    'La paeu de chargin', 
+    new Date(),
+    '/images/books/la_paeu_de_chagrin.jpg',
+    314, 
+    'Prosvetno delo',
+    'MKD', 
+    'drama', 
+    '$10'
+);
+
+console.log(chargin);
