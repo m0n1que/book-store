54e132dd70b4afe9249d715a4dde6adc2eec8146
Monika <jovanova02monika@gmail.com>
2020-06-04 14:31:46 +0200

    Load data from json

--- scripts/app.js
+++ scripts/app.js
@@ -28,72 +28,63 @@ class Book extends Author{
     }
 } 
 
-const bookStaticList = [
-    {
-        "Author": {
-            "fName": "Steve", 
-            "lName": "Berry"
-        },
-        "title": "The Alexandria Link",
-        "publishedDate": "2007", 
-        "picture": "images/books/alexandria_link.jpg",
-        "totalPages": 462,
-        "publisher": "Ballantine Books",
-        "language":"English",
-        "category": "Mistery",
-        "price": "$10", 
-        "type": "best-selling"
-    },
-    {
-        "Author": {
-            "fName": "Steve", 
-            "lName": "Berry"
-        },
-        "title": "The Venetian Betrayal",
-        "publishedDate": "2008", 
-        "picture": "images/books/venetian_betrayal.jpg",
-        "totalPages": 462,
-        "publisher": "Ballantine Books",
-        "language":"English",
-        "category": "Mistery",
-        "price": "$10",
-        "type": "best-selling"
-    },
-    {
-        "Author": {
-            "fName": "Jordan", 
-            "lName": "Peterson"
-        },
-        "title": "12 Rules for Life",
-        "publishedDate": "2018", 
-        "picture": "images/books/12_rules_of_life.jpg",
-        "totalPages": 448,
-        "publisher": "Random House Canada",
-        "language":"English",
-        "category": "Self-help book",
-        "price": "$15",
-        "type": "latest-books"
-    }
-];
-
 class BookList {
     books = [];
 
     constructor(type) {
         this.type = type;
-        const books = bookStaticList.filter(book => book.type == type);
-        this.createBookItem(books, type);
+        if (this.type != 'already-selling'){
+            this.loadBooks(type);
+        }
+    }
+
+    loadBooks(type) {
+        fetch('http://localhost:5000/seed_data/data.json')
+            .then(response => response.json())
+            .then(data => {
+                this.books = data.filter(book => book.type == type);
+                this.createBookItem(this.books, type);
+            });
+
     }
 
     createBookItem(books, type) {
-        const bookTypes = document.querySelector(`#main section .row .${type}`);
-        const cloned = bookTypes.cloneNode(true);
-        const div_book_to_add = bookTypes.parentNode;
-        for (const item of books) {
-            cloned.childNodes[1].childNodes[1].src = item.picture;
-            cloned.childNodes[1].childNodes[1].alt = item.title;
+        // const bookTypes = document.querySelector(`#main section .row .${type}`);
+        // const cloned = bookTypes.cloneNode(true);
+        // const div_book_to_add = bookTypes.parentNode;
+        // for (const item of books) {
+        //     cloned.childNodes[1].childNodes[1].src = item.picture;
+        //     cloned.childNodes[1].childNodes[1].alt = item.title;
+        //     div_book_to_add.append(cloned);
+        // }
+        const typeDiv = document.querySelector(`#${type}`);
+        for(const item of books) {
+            const wrapDiv = document.createElement('DIV');
+            wrapDiv.classList.add('col-md-3', 'col-sm-6', 'best-selling');
+            const cardBook = document.createElement('DIV');
+            cardBook.classList.add('card-body');
+            wrapDiv.append(cardBook);
+    
+            const imgBook = document.createElement('IMG');
+            imgBook.src = item.picture;
+            imgBook.alt = item.title;
+            cardBook.append(imgBook);
+
+            const footerBook = document.createElement('DIV');
+            footerBook.classList.add('d-flex', 'justify-content-center', 'align-items-center');
+            cardBook.append(footerBook);
+
+            const btnHolder = document.createElement('DIV');
+            btnHolder.classList.add('btn-group');
+            const viewBtn = document.createElement('BUTTON');
+            viewBtn.classList.add('btn', 'btn-lg', 'btn-outline-secondary', 'btn-best-sell');
+            const btnText = document.createTextNode("View");
+            viewBtn.append(btnText);
+            btnHolder.append(viewBtn);
+            footerBook.append(btnHolder);
+            typeDiv.append(wrapDiv);
         }
-        bookTypes.parentNode.append(cloned);
+
     }
 }
 
@@ -101,7 +92,7 @@ class App {
     static init() {
         const bestSellerBooks = new BookList('best-selling');
         const lastestBooks = new BookList('latest-books');
-    }
+    }    
 }
 
 App.init();
