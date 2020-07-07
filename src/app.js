14983936175d02e99e3ad4a9fe6d4171c6b4c018
Monika <jovanova02monika@gmail.com>
2020-07-07 16:33:28 +0200

    book classes implementation

--- src/app.js
+++ src/app.js
@@ -1,92 +1,86 @@
-// class Author {
-//     constructor(fName, lName) {
-//         this.fName = fName;
-//         this.lName = lName;
-//     }
-// }
-
-// class Book extends Author{
-//     title = '';
-//     publishedDate = new Date(); 
-//     picture = '';
-//     totalPages; 
-//     publisher; 
-//     language; 
-//     category;
-//     price = 0;
-
-//     constructor(fName, lName, title, publishedDate, picture, totalPages, publisher, language, category, price) {
-//         super(fName, lName);
-//         this.title = title;
-//         this.publishedDate = new Date(); 
-//         this.picture = picture;
-//         this.totalPages = totalPages; 
-//         this.publisher = publisher; 
-//         this.language = language;
-//         this.category = category;
-//         this.price = price;
-//     }
-// } 
-
-class BookList {
-    //books = [];
-
-    constructor(type) {
-        this.type = type;
-        this.books = [];
-        if (this.type != 'already-selling'){
-            this.loadBooks(type);
-        }
-    }
-
-    loadBooks(type) {
-        fetch('https://localhost:8080/seed_data/data.json')
-            .then(response => response.json())
-            .then(data => {
-                this.books = data.filter(book => book.type == type);
-                this.createBookItem(this.books, type);
-            });
-
-    }
-
-    createBookItem(books, type) {
-        const typeDiv = document.querySelector(`#${type}`);
-        for(const item of books) {
-            const wrapDiv = document.createElement('DIV');
-            wrapDiv.classList.add('col-md-3', 'col-sm-6', 'best-selling');
-            const cardBook = document.createElement('DIV');
-            cardBook.classList.add('card-body');
-            wrapDiv.append(cardBook);
-    
-            const imgBook = document.createElement('IMG');
-            imgBook.src = item.picture;
-            imgBook.alt = item.title;
-            cardBook.append(imgBook);
-
-            const footerBook = document.createElement('DIV');
-            footerBook.classList.add('d-flex', 'justify-content-center', 'align-items-center');
-            cardBook.append(footerBook);
-
-            const btnHolder = document.createElement('DIV');
-            btnHolder.classList.add('btn-group');
-            const viewBtn = document.createElement('BUTTON');
-            viewBtn.classList.add('btn', 'btn-lg', 'btn-outline-secondary', 'btn-best-sell');
-            const btnText = document.createTextNode("View");
-            viewBtn.append(btnText);
-            btnHolder.append(viewBtn);
-            footerBook.append(btnHolder);
-            typeDiv.append(wrapDiv);
-        }
-
+class Book{
+    constructor(id, title, publishDate, picture, totlaPages, publisher, language, category, price, type) {
+        this.id = id; 
+        this.title = title; 
+        this.publishedDate = publishDate; 
+        this.picture = picture;
+        this.totalPages = totlaPages;
+        this.publisher = publisher;
+        this.language = language; 
+        this.category = category; 
+        this.price = price; 
+        this.type = type; 
     }
 }
 
+class BookList{
+    constructor(type) {
+        this.books = [];
+        this.type = type;
+        this.loadBooks(type);
+    }
+
+    loadBooks(type){
+        fetch('https://localhost:8080/seed_data/data.json')
+            .then(response => response.json())
+            .then(data => {
+                let filterBooks = data.filter(book => book.type == type);
+                for(let i of filterBooks){
+                    let filteredBook = new Book(i.id, i.title, i.publishedDate, i.picture, i.totlaPages, i.publisher, i.language, i.category, i.price, i.type);
+                    this.books.push(filteredBook)
+                }
+                this.render(type);
+            });
+    }
+
+    render(type) {
+        const bookList = document.getElementById(type);        
+        for (const bks of this.books){
+            const bookItem = new BookItem(bks);
+            const bkEl = bookItem.renderBookItem();
+            bookList.append(bkEl);
+        }
+        return bookList;
+    }   
+}
+
+class BookItem {
+    constructor(book){
+        this.book = book;
+    }
+
+    viewBookDetails(){
+        console.log(this.book);
+        console.log('viewing the book details');
+    }
+
+    renderBookItem(){
+        const bkEl = document.createElement('div');
+        bkEl.classList.add('col-md-3', 'col-sm-6', 'best-selling');
+        bkEl.innerHTML = `
+            <div class="card-body">
+                <img src="${this.book.picture}" alt="${this.book.title}">
+            </div>
+            <div class="d-flex justify-content-center align-items-center">
+                <div class="btn-group">
+                    <buttton id="btn_view" class="btn btn-lg btn-outline-secondary btn-best-sell">View</button>
+                </div>
+            </div>
+        `;
+        const addViewButton = bkEl.querySelector('#btn_view');
+        addViewButton.addEventListener('click', this.viewBookDetails.bind(this));
+        return bkEl;
+    }
+}
+
+
 class App {
     static init() {
-        const bestSellerBooks = new BookList('best-selling');
-        const lastestBooks = new BookList('latest-books');
-        console.log(lastestBooks);
-        console.log(bestSellerBooks);
+
+        const renderHook = document.getElementById('main');
+        const latestBooks = new BookList('latest-books');
+        const bestSelling = new BookList('best-selling');
+
         this.getCurrentUser();
     }    
 
@@ -99,20 +93,19 @@ class App {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${bks_token_ex}`
                 }
-            }).then(response => response.json())
-              .then(data => {
-                  const userLbl = document.getElementById('user_in');
-                  userLbl.textContent = data.displayName;
-                  document.getElementById('login_link').style.display = 'none';
-                  document.getElementById('register_link').style.display = 'none';
-                  document.getElementById('profile').style.display = 'block';
-              });
+            })
+            .then(response => response.json())
+            .then(data => {
+                const userLbl = document.getElementById('user_in');
+                userLbl.textContent = data.displayName;
+                document.getElementById('login_link').style.display = 'none';
+                document.getElementById('register_link').style.display = 'none';
+                document.getElementById('profile').style.display = 'block';
+            });
         }
     }
 }
-
 App.init();
-
 
 const logout = document.getElementById('logout_link');
 logout.addEventListener('click', (e)=>{
